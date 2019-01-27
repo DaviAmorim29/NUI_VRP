afbf407f1fc9444a526d095bec3f539d4a3e26eb
Amorim <daviamorim29@hotmail.com>
2019-01-27 04:42:29 -0200

    Update click.lua

--- Old vRP/vrp_ilegal/click.lua
+++ Old vRP/vrp_ilegal/click.lua
@@ -1,1 +1,108 @@
+-- ###################################
+--
+--       Credits: Shadow
+--
+-- ###################################
+local Tunnel = module("vrp", "lib/Tunnel")
+local Proxy = module("vrp", "lib/Proxy")
 
+vRP = Proxy.getInterface("vRP")
+vRPclient = Tunnel.getInterface("vRP")
+JBserver = Tunnel.getInterface("vrp_ilegal","vrp_ilegal")
+
+local menuEnabled = false
+
+RegisterNetEvent("ToggleActionmenu")
+AddEventHandler("ToggleActionmenu", function()
+	ToggleActionMenu()
+end)
+
+function ToggleActionMenu()
+	Citizen.Trace("tutorial launch")
+	menuEnabled = not menuEnabled
+	if ( menuEnabled ) then
+		SetNuiFocus( true, true )
+		SendNUIMessage({
+			showPlayerMenu = true
+		})
+	else
+		SetNuiFocus( false )
+		SendNUIMessage({
+			showPlayerMenu = false
+		})
+	end
+end
+
+function killTutorialMenu()
+SetNuiFocus( false )
+		SendNUIMessage({
+			showPlayerMenu = false
+		})
+		menuEnabled = false
+
+end
+
+
+RegisterNUICallback('close', function(data, cb)
+  ToggleActionMenu()
+  cb('ok')
+end)
+
+Citizen.CreateThread(function ()
+	while true do
+		Citizen.Wait(0)
+		DrawMarker(27, 705.682189941406,-966.919067382813,30.3953418731689-1.0001, 0, 0, 0, 0, 0, 0, 1.0001,1.0001,1.0001, 0, 232, 255, 155, 0, 0, 0, 0, 0, 0, 0)
+		if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), 705.682189941406,-966.919067382813,30.3953418731689, true ) < 1 then
+			DisplayHelpText("Pressione ~g~E~s~ para acessar a agencia de empregos")
+		 if (IsControlJustReleased(1, 51)) then
+			SetNuiFocus( true, true )
+			SendNUIMessage({
+				showPlayerMenu = true
+			})
+		 end
+		end
+	end
+end)
+
+RegisterNUICallback('desempregado', function(data, cb)
+	TriggerClientEvent("desempregado")
+  	cb('ok')
+end)
+
+RegisterNUICallback('ladraocarro', function(data, cb)
+	TriggerClientEvent("ladraocarro")
+  	cb('ok')
+end)
+
+RegisterNUICallback('traficante', function(data, cb)
+	TriggerClientEvent("traficante")
+  	cb('ok')
+end)
+
+RegisterNUICallback('traficatortuga', function(data, cb)
+	TriggerClientEvent("traficatortuga")
+  	cb('ok')
+end)
+
+RegisterNUICallback('hacker', function(data, cb)
+	TriggerClientEvent("hacker")
+  	cb('ok')
+end)
+
+
+RegisterNUICallback('closeButton', function(data, cb)
+	killTutorialMenu()
+  	cb('ok')
+end)
+
+function DrawSpecialText(m_text, showtime)
+	SetTextEntry_2("STRING")
+	AddTextComponentString(m_text)
+	DrawSubtitleTimed(showtime, 1)
+end
+
+function DisplayHelpText(str)
+	SetTextComponentFormat("STRING")
+	AddTextComponentString(str)
+	DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
